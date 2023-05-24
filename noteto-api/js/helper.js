a86ceba646a1d6fc4044a1a7f1af80ccf50d741e
Tee Nguov <Tee.Nguov@JaekJay.com>
2023-05-23 21:05:30 -0700

    second fix

--- noteto-api/js/helper.js
+++ noteto-api/js/helper.js
@@ -200,7 +200,7 @@ function verifyToken(req, res, next) {
 }
 //=======================================================================================
 function getEntryText(field, fldVal) {
-  if (!fldVal || Object.keys(fldVal).length == 0) {
+  if (_.isEmpty(fldVal)) {
     return "";
   }
   if (field.type == "multipleSelect") {
