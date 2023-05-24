fcf2aad1e6c2555b2322488d27a13ab2424cdd01
Tee Nguov <Tee.Nguov@JaekJay.com>
2023-05-23 21:12:33 -0700

    no message

--- noteto-api/js/helper.js
+++ noteto-api/js/helper.js
@@ -200,7 +200,7 @@ function verifyToken(req, res, next) {
 }
 //=======================================================================================
 function getEntryText(field, fldVal) {
-  if (!_.isNumeric(fldVal) && f_.isEmpty(fldVal)) {
+  if (!_.isNumber(fldVal) && f_.isEmpty(fldVal)) {
     return "";
   }
   if (field.type == "multipleSelect") {
