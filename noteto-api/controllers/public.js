f01cfd9b3bf53d06e86e858ed62c33ae25d1622b
Tee Nguov <Tee.Nguov@JaekJay.com>
2023-06-26 17:30:20 -0700

    updated public controller

--- noteto-api/controllers/public.js
+++ noteto-api/controllers/public.js
@@ -10,71 +10,78 @@ const projectPath = process.cwd();
 let dbConn = null;
 
 function setDb(conn) {
-  publicService.setDb(conn);
-  userService.setDb(conn);
+	publicService.setDb(conn);
+	userService.setDb(conn);
 }
 app.use(cors());
 app.use(express.json());
 app.get("/receipt", (req, res) => {
-  res.sendFile(
-    path.join(
-      projectPath + `/custom/receipts/${req.query.database}-receipt.html`
-    )
-  );
+	res.sendFile(
+		path.join(
+			projectPath + `/custom/receipts/${req.query.database}-receipt.html`
+		)
+	);
 });
 app.get("/api/get/entry/:id", (req, res) => {
-  publicService.getReceiptById(req.params.id).then((response) => {
-    res.status(response.code).send(response.data);
-  });
+	publicService.getReceiptById(req.params.id).then((response) => {
+		res.status(response.code).send(response.data);
+	});
+});
+app.get("/api/get/entries/by/database/:database", (req, res) => {
+	publicService.getEntriesByDatabase(req.params.database).then((response) => {
+		res.status(response.code).send(response.data);
+	});
 });
 app.get("/dashboard", (req, res) => {
-  res.sendFile(
-    path.join(__dirname + `/dashboards/${req.query.database}-dashboard.html`)
-  );
+	res.sendFile(
+		path.join(
+			__dirname + `/dashboards/${req.query.database}-dashboard.html`
+		)
+	);
 });
 app.get("/assets/:name", (req, res) => {
-  let options = {
-    root: path.join(__dirname, "receipts/assets"),
-    dotfiles: "deny",
-    headers: {
-      "x-timestamp": Date.now(),
-      "x-sent": true,
-    },
-  };
-  let pictureName = req.params.name;
-  res.sendFile(pictureName, options, function (err) {
-    if (err) {
-      res.sendStatus(500);
-      return console.log("Unable to send file");
-    }
-  });
+	let options = {
+		root: path.join(__dirname, "receipts/assets"),
+		dotfiles: "deny",
+		headers: {
+			"x-timestamp": Date.now(),
+			"x-sent": true,
+		},
+	};
+	let pictureName = req.params.name;
+	res.sendFile(pictureName, options, function (err) {
+		if (err) {
+			res.sendStatus(500);
+			return console.log("Unable to send file");
+		}
+	});
 });
 app.get(
-  "/api/verify/by/session/:sessionId",
-  helper.verifyEmailToken,
-  (req, res) => {
-    userService
-      .markVerifiedByEmail(req.decoded.email)
-      .then((response) => {
-        res.sendStatus(response.code);
-      })
-      .catch((response) => {
-        res.status(response.code).send(response.message);
-      });
-  }
+	"/api/verify/by/session/:sessionId",
+	helper.verifyEmailToken,
+	(req, res) => {
+		userService
+			.markVerifiedByEmail(req.decoded.email)
+			.then((response) => {
+				res.sendStatus(response.code);
+			})
+			.catch((response) => {
+				res.status(response.code).send(response.message);
+			});
+	}
 );
 app.get("/send/email", (req, res) => {
-  mailService
-    .sendAccountVerifyEmail({
-      first: "Tee",
-      last: "Nguov",
-      email: "kimlengng23@gmail.com",
-    })
-    .then((response) => {
-      res.sendStatus(response.code);
-    });
+	mailService
+		.sendAccountVerifyEmail({
+			first: "Tee",
+			last: "Nguov",
+			email: "kimlengng23@gmail.com",
+		})
+		.then((response) => {
+			res.sendStatus(response.code);
+		});
 });
 module.exports = {
-  app,
-  setDb,
+	app,
+	setDb,
 };
--- noteto-api/services/public.js
+++ noteto-api/services/public.js
@@ -3,35 +3,76 @@ const { ObjectId } = require("mongodb");
 const fieldService = require("./field.js");
 let dbConn = null;
 function setDb(conn) {
-  dbConn = conn;
-  helper.setDb(conn);
-  fieldService.setDb(conn);
+	dbConn = conn;
+	helper.setDb(conn);
+	fieldService.setDb(conn);
 }
 function getReceiptById(id) {
-  let promise = new Promise((resolve, reject) => {
-    dbConn
-      .collection("EntryCollection")
-      .findOne({ _id: new ObjectId(id) }, (err, result) => {
-        if (err) {
-          console.log("EntryService - getEntryById", err);
-          reject({ code: 500, message: err });
-        } else {
-          fieldService.getFieldsByDatabase(result.database).then((response) => {
-            let fields = response.data;
-            let entry = {};
-            for (let i = 0; i < fields.length; i++) {
-              let field = fields[i];
-              let fldVal = result[field.value];
-              entry[field.value] = helper.getEntryText(field, fldVal);
-            }
-            resolve({ code: 200, data: entry });
-          });
-        }
-      });
-  });
-  return promise;
+	let promise = new Promise((resolve, reject) => {
+		dbConn
+			.collection("EntryCollection")
+			.findOne({ _id: new ObjectId(id) }, (err, result) => {
+				if (err) {
+					console.log("EntryService - getEntryById", err);
+					reject({ code: 500, message: err });
+				} else {
+					fieldService
+						.getFieldsByDatabase(result.database)
+						.then((response) => {
+							let fields = response.data;
+							let entry = {};
+							for (let i = 0; i < fields.length; i++) {
+								let field = fields[i];
+								let fldVal = result[field.value];
+								entry[field.value] = helper.getEntryText(
+									field,
+									fldVal
+								);
+							}
+							resolve({ code: 200, data: entry });
+						});
+				}
+			});
+	});
+	return promise;
+}
+function getEntriesByDatabase(database) {
+	console.log(database);
+	let promise = new Promise((resolve, reject) => {
+		dbConn
+			.collection("EntryCollection")
+			.find({ database: database })
+			.toArray((err, results) => {
+				if (err) {
+					console.log("EntryService - getEntriesByDatabase", err);
+					reject({ code: 500, message: err });
+				} else {
+					let entries = [];
+					fieldService
+						.getFieldsByDatabase(database)
+						.then((response) => {
+							for (let i = 0; i < results.length; i++) {
+								let fields = response.data;
+								let entry = {};
+								for (let i = 0; i < fields.length; i++) {
+									let field = fields[i];
+									let fldVal = results[i][field.value];
+									entry[field.value] = helper.getEntryText(
+										field,
+										fldVal
+									);
+								}
+								entries.push(entry);
+							}
+							resolve({ code: 200, data: entries });
+						});
+				}
+			});
+	});
+	return promise;
 }
 module.exports = {
-  setDb,
-  getReceiptById,
+	setDb,
+	getEntriesByDatabase,
+	getReceiptById,
 };
